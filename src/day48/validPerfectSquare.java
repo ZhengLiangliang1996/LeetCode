a27e852eb4f58dbf1092a072b9d34672eb78ddc5
ZhengLiangliang1996 <zhengliangliang1996@gmail.com>
2018-11-16 23:37:57 +0100

    day 48

---
+++ src/day48/validPerfectSquare.java
@@ -0,0 +1,27 @@
+package day48;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+public class validPerfectSquare {
+	
+    public static boolean isPerfectSquare(int num) {
+//    	if(num == 1){
+//    		return true;
+//    	}
+//    	if(num >= 4){
+//    		for (int i = 2; i <= num/2; i++) {
+//				if(i * i == num)
+//					return true;
+//			}
+//        }
+//        return false;
+    	
+    	double s = Math.sqrt(num);
+    	return (s - Math.floor(s) == 0) ? true : false;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(isPerfectSquare(14));
+	}
+	
+}
